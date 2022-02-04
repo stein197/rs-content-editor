96b35df176bf8717037353cfbe23718bedc7d4c5
Nail' Gafarov <nil20122013@gmail.com>
2022-02-04 20:09:12 +0500

    Changed dependency creation

--- config/definition.php
+++ config/definition.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\RouteBuilder;
+use function DI\create;
+use function DI\get;
 use function App\resolvePath;
 
 return [
-	RouteBuilder::class => new RouteBuilder(require resolvePath('config/route.php')),
+	RouteBuilder::class => create()->constructor(get('config.route')),
 	'request.method' => $_SERVER['REQUEST_METHOD'],
 	'request.uri' => $_SERVER['REQUEST_URI'],
+	'config.route' => fn () => require resolvePath('config/route.php')
 ];

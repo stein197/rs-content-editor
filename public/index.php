414ee7398c128244ae8fe76bf1b0134b5097215a
Nail' Gafarov <nil20122013@gmail.com>
2022-02-04 22:18:49 +0500

    Removed redundant doc comment

--- public/index.php
+++ public/index.php
@@ -4,7 +4,6 @@ require __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'vendor'.DIRECTORY_
 use DI\Container;
 use Psr\Http\Message\RequestInterface;
 use App\Router;
-use App\RouteHandler;
 use function App\container;
 use function App\sendResponse;
 
@@ -15,7 +14,6 @@ error_reporting(E_ALL ^ E_DEPRECATED);
 	$request = $container->get(RequestInterface::class);
 	/** @var Router */
 	$router = $container->get(Router::class);
-	/** @var RouteHandler */
 	$handler = $router->dispatch($request->getMethod(), explode('?', (string) $request->getUri())[0]);
 	$response = $handler->handle($request);
 	sendResponse($response);

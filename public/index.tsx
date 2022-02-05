28ac5683ebded58a21dce4337d62235439f75614
Nail' Gafarov <nil20122013@gmail.com>
2022-02-05 11:04:07 +0500

    Moved stylesheed to index.tsx

--- public/index.tsx
+++ public/index.tsx
@@ -1,6 +1,7 @@
 import * as ReactDOM from "react-dom";
 import * as React from "react";
 import App from "App";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 document.addEventListener("DOMContentLoaded", () => {
 	ReactDOM.render(<App/>, document.body.querySelector("main"));
--- public/src/App.tsx
+++ public/src/App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import {Container, Row, Col} from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function App(): JSX.Element {
 	return (
 		<Container>
-			<Row className="">
-				<Col>
-				</Col>
+			<Row className="d-flex">
+				<Col>Abc</Col>
 			</Row>
 		</Container>
 	);

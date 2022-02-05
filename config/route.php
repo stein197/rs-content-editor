fb815834e0c3a72558e1817e2cc23a0215e27155
Nail' Gafarov <nil20122013@gmail.com>
2022-02-05 20:05:48 +0500

    -class(App\Middleware\OutputHtml)

--- config/route.php
+++ config/route.php
@@ -1,23 +1,15 @@
 <?php
 
-use Psr\Http\Message\RequestInterface;
-use Psr\Http\Message\ResponseInterface;
 use App\Controller\Index;
+use App\Controller\HtmlStatic;
 use App\Middleware\LoadDatabase;
 use App\Middleware\LoadDotEnv;
-use App\Middleware\OutputHtml;
 use App\Middleware\OutputJson;
 use App\RouteBuilder;
-use function App\resolvePath;
 
 return function (RouteBuilder $r) {
 	$r->before(OutputJson::class, LoadDotEnv::class, LoadDatabase::class)->group('/api', function (RouteBuilder $r): void {
 		$r->get('/', Index::class);
 	});
-	$r->before(OutputHtml::class)->get('{path:.+}', function (RequestInterface $request, ResponseInterface $response): ResponseInterface {
-		ob_start();
-		require resolvePath('public/index.html');
-		$response->getBody()->write(ob_get_clean());
-		return $response;
-	});
+	$r->get('/', HtmlStatic::class);
 };
--- src/Controller/HtmlStatic.php
+++ src/Controller/HtmlStatic.php
@@ -1,14 +1,17 @@
 <?php
-
-namespace App\Middleware;
+namespace App\Controller;
 
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use App\Controller;
+use function App\resolvePath;
 
-class OutputHtml extends Controller {
+class HtmlStatic extends Controller {
 
 	public function handle(RequestInterface $request, ResponseInterface $response, array $requestVars): ResponseInterface {
+		ob_start();
+		require resolvePath('public/index.html');
+		$response->getBody()->write(ob_get_clean());
 		return $response->withHeader('Content-Type', 'text/html');
 	}
 }

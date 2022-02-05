2cdf54303365235dc4caf1ebf91c342b14f2979f
Nail' Gafarov <nil20122013@gmail.com>
2022-02-05 16:37:13 +0500

    Deleted if-then-else

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 import {Container, Row, Col, Form, Button, Card, Alert} from "react-bootstrap";
-import If from "view/flow/If";
-import Then from "view/flow/Then";
-import Else from "view/flow/Else";
 import Fetch from "view/Fetch";
 
 export default function App() {
@@ -10,32 +7,29 @@ export default function App() {
 		<Fetch input="/api/" json={true}>{(response, json) => (
 			<Container>
 				<Row className="justify-content-center">
-					<Col md={8} lg={6}>
-						<If test={response.ok}>
-							<Then>
-								<Card className="m-1 m-md-3 m-lg-5">
-									<Card.Body>
-										<Form>
-											<Form.Group className="mb-3">
-												<Form.Label>Логин</Form.Label>
-												<Form.Control type="text" placeholder="Логин" name="login"/>
-											</Form.Group>
-											<Form.Group className="mb-3">
-												<Form.Label>Пароль</Form.Label>
-												<Form.Control type="password" placeholder="Пароль" name="password"/>
-											</Form.Group>
-											<Button variant="primary" type="submit" className="w-100">Войти</Button>
-										</Form>
-									</Card.Body>
-								</Card>
-							</Then>
-							<Else>
-								<Alert variant="danger" className="m-1 m-md-3 m-lg-5">
-									{json.error.msg}
-								</Alert>
-							</Else>
-						</If>
-					</Col>
+					<Col md={8} lg={6}>{
+						response.ok ? (
+							<Card className="m-1 m-md-3 m-lg-5">
+								<Card.Body>
+									<Form>
+										<Form.Group className="mb-3">
+											<Form.Label>Логин</Form.Label>
+											<Form.Control type="text" placeholder="Логин" name="login"/>
+										</Form.Group>
+										<Form.Group className="mb-3">
+											<Form.Label>Пароль</Form.Label>
+											<Form.Control type="password" placeholder="Пароль" name="password"/>
+										</Form.Group>
+										<Button variant="primary" type="submit" className="w-100">Войти</Button>
+									</Form>
+								</Card.Body>
+							</Card>
+						) : (
+							<Alert variant="danger" className="m-1 m-md-3 m-lg-5">
+								{json.error.msg}
+							</Alert>
+						)
+					}</Col>
 				</Row>
 			</Container>
 		)}</Fetch>

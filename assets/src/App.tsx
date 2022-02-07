94936bb933bfa9aa2f0ba6b52f855571f87b6a4a
Nail' Gafarov <nil20122013@gmail.com>
2022-02-07 22:38:50 +0500

    d(assets/src/page -> assets/src/view/page)

--- assets/src/App.tsx
+++ assets/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {HashRouter, Routes, Route} from "react-router-dom";
-import Index from "page/Index";
-import NotFound from "page/NotFound";
+import Index from "view/page/Index";
+import NotFound from "view/page/NotFound";
 
 export default function App(): JSX.Element {
 	return window.location.pathname === "/" ? (

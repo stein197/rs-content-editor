10f2d261ab026494d4ca0c1b313cd6c85a504f99
Nail' Gafarov <nil20122013@gmail.com>
2022-02-08 16:21:51 +0500

    Added file(), download() and headers() methods

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -5,6 +5,8 @@ use GuzzleHttp\Psr7\Utils;
 use Psr\Http\Message\ResponseInterface;
 use App\Http\TerminateException;
 use App\View;
+use function basename;
+use function App\resolvePath;
 
 class Response {
 
@@ -18,8 +20,19 @@ class Response {
 		return $this->body((new View($name, $data))->render());
 	}
 
-	// TODO
-	public function file(string $path): self {}
+	public function file(string $path): self {
+		$path = resolvePath($path);
+		return $this->header('Content-Type', mime_content_type($path))->body(file_get_contents($path));
+	}
+
+	public function download(string $path, ?string $name = null): self {
+		$name = $name ?? basename($path);
+		return $this->file(resolvePath($path))->headers([
+			'Content-Type' => 'application/octet-stream',
+			'Content-Transfer-Encoding' => 'Binary',
+			'Content-Disposition' => "attachment; filename=\"{$name}\"",
+		]);
+	}
 
 	public function status(int $status): self {
 		return new self($this->response->withStatus($status));
@@ -38,6 +51,13 @@ class Response {
 
 	public function header(string $key, null | string | array $value): self {
 		return new self($value ? $this->response->withHeader($key, $value) : $this->response->withoutHeader($key));
+	}
+
+	public function headers(array $values): self {
+		$response = $this;
+		foreach ($values as $key => $value)
+			$response = $response->header($key, $value);
+		return $response;
 	}
 
 	public function body($resource): self {

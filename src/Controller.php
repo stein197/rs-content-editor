252ae1f371eb45de0c4468c7f51da9e47bab3d97
Nail' Gafarov <nil20122013@gmail.com>
2022-02-11 14:01:51 +0500

    -m(App\Controller::validate())

--- src/Controller.php
+++ src/Controller.php
@@ -33,7 +33,4 @@ abstract class Controller {
 		$method = strtolower($request->psr()->getMethod());
 		return in_array(strtoupper($method), HTTP_METHODS) && method_exists($this, $method) ? $this->{$method}($request, $response) : $response;
 	}
-
-	// TODO
-	protected function validate(Request $request, Response $response): Response {}
 }

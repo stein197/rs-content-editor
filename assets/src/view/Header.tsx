b43c2187147a0c0deab0d71e396d8453a49095ba
Nail' Gafarov <nil20122013@gmail.com>
2022-02-13 15:38:11 +0500

    Added import dialog

--- assets/src/view/Header.tsx
+++ assets/src/view/Header.tsx
@@ -1,37 +1,62 @@
 import React from "react";
-import {Button, Form, Card} from "react-bootstrap";
+import {Button, Form, Card, Modal} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import Fetch from "view/Fetch";
 import URL from "API/URL";
 
 export default function Header(): JSX.Element {
+	const [importVisible, setImportVisible] = React.useState(false);
+	const onImportClick = React.useCallback(() => {
+		setImportVisible(true);
+	}, []);
+	const onModalCloseClick = React.useCallback(() => {
+		setImportVisible(false);
+	}, []);
+	const onExportClick = React.useCallback(() => {
+
+	}, []);
+	
 	return (
-		<Fetch input={URL.User}>
-			{React.useCallback((response, data) => (
-				<header>
-					<Card>
-						<Card.Body className="d-flex flex-row justify-content-end">
-							<Button variant="primary" className="m-1">
-								<Link to="/" className="text-white">Главная</Link>
-							</Button>
-							<Button variant="primary" className="m-1">
-								<Link to="/import/" className="text-white">Импорт</Link>
-							</Button>
-							<Button variant="primary" className="m-1">
-								<Link to="/export/" className="text-white">Экспорт</Link>
-							</Button>
-							{data.admin > 0 && (
+		<>
+			<Modal show={importVisible}>
+				<Modal.Header closeButton>
+					<Modal.Title>Импорт данных</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>Внимание! При импорте данных, вся база данных будет очищена</Modal.Body>
+				<Modal.Footer>
+					<Button variant="primary">
+						<label>
+							<input type="file" style={{display: "none"}}/>
+							<span>Продолжить</span>
+						</label>
+					</Button>
+					<Button variant="danger" onClick={onModalCloseClick}>Закрыть</Button>
+				</Modal.Footer>
+			</Modal>
+			<Fetch input={URL.User}>
+				{React.useCallback((response, data) => (
+					<header>
+						<Card>
+							<Card.Body className="d-flex flex-row justify-content-end">
 								<Button variant="primary" className="m-1">
-									<Link to="/users/" className="text-white">Пользователи</Link>
+									<Link to="/" className="text-white">Главная</Link>
 								</Button>
-							)}
-							<Form action="/logout/" method="POST" className="m-1">
-								<Button type="submit" variant="dark">Выйти</Button>
-							</Form>
-						</Card.Body>
-					</Card>
-				</header>
-			), [])}
-		</Fetch>
+								<Button variant="primary" className="m-1" onClick={onImportClick}>Импорт</Button>
+								<Button variant="primary" className="m-1" onClick={onExportClick}>Экспорт</Button>
+								{data.admin > 0 && (
+									<Button variant="primary" className="m-1">
+										<Link to="/users/" className="text-white">Пользователи</Link>
+									</Button>
+								)}
+								<Form action="/logout/" method="POST" className="m-1">
+									<Button type="submit" variant="dark">Выйти</Button>
+								</Form>
+							</Card.Body>
+						</Card>
+					</header>
+				), [])}
+			</Fetch>
+
+		</>
 	);
 }

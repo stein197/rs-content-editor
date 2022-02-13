c2619b9a7aa7589574c26ea4d525520760623f17
Nail' Gafarov <nil20122013@gmail.com>
2022-02-13 18:19:04 +0500

    Basic json encoding

--- src/Controller/Import.php
+++ src/Controller/Import.php
@@ -2,14 +2,28 @@
 
 namespace App\Controller;
 
+use JsonException;
 use App\Controller;
 use App\Http\Request;
 use App\Http\Response;
+use App\Http\Status;
+use function json_decode;
+use const JSON_THROW_ON_ERROR;
 
 // TODO
 class Import extends Controller {
 
 	public function post(Request $request, Response $response): Response {
+		$this->app->db()->truncateData();
+		try {
+			$data = json_decode($request->psr()->getBody()->getContents(), false, JSON_THROW_ON_ERROR);
+		} catch (JsonException $ex) {
+			return $response->json([
+				'error' => [
+					'message' => $ex->getMessage()
+				]
+			])->status(Status::BAD_REQUEST);
+		}
 		return $response;
 	}
 }
--- src/Http/Status.php
+++ src/Http/Status.php
@@ -4,6 +4,7 @@ namespace App\Http;
 final class Status {
 
 	public const MOVED_PERMANENTLY = 301;
+	public const BAD_REQUEST = 400;
 	public const NOT_FOUND = 404;
 	public const METHOD_NOT_ALLOWED = 405;
 

e87c8ca5575301da3b2d78c2941c0aebefe2a1ae
Nail' Gafarov <nil20122013@gmail.com>
2022-03-24 15:16:38 +0500

    Added active menu entry highlighting

--- assets/src/view/TypeMenuItem.tsx
+++ assets/src/view/TypeMenuItem.tsx
@@ -1,19 +1,24 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import Fetch from "view/Fetch";
 
 export default function TypeMenuItem(props: TypeMenuItemProps): JSX.Element {
+	const params = useParams();
 	return (
 		<Fetch input={`/api/types/${props.id}/`}>
 			{(resource, data) => (
-				<ul>
-					{data.map((item: any) => (
-						<li key={item.id}>
-							<Link to={`/${item.id}/`}>{item.name}</Link>
-							<TypeMenuItem id={item.id}/>
-						</li>
-					))}
-				</ul>
+				data.length > 0 ? (
+					<ul>
+						{data.map((item: any) => (
+							<li key={item.id}>
+								<Link to={`/${item.id}/`} className={params.typeID == item.id ? "active" : ""}>{item.name}</Link>
+								<TypeMenuItem id={item.id}/>
+							</li>
+						))}
+					</ul>
+				) : (
+					null
+				)
 			)}
 		</Fetch>
 	);
